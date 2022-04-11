aac5b325516c9c666ab6c64caf2ed2d68f62f568
Shashank Karan <shashankkaran999@gmail.com>
2022-04-11 23:11:22 +0530

    tailwind added

--- src/Pages/franchise/Franchise.js
+++ src/Pages/franchise/Franchise.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 function Franchise() {
-  return (
-    <div>Franchise</div>
-  )
+    return (
+        <div >
+            <h1 style={{textAlign:'center',fontSize:'5rem'}}>Franchise</h1>
+        </div>
+    )
 }
 
 export default Franchise
