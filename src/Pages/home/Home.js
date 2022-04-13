f61080ddc1e34a673020644a0387ba83523c981d
Shashank Karan <shashankkaran999@gmail.com>
2022-04-13 23:04:49 +0530

    bootstrap integrated

--- src/Pages/home/Home.js
+++ src/Pages/home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Services from '../services/Services'
+import SubHome from './SubHome'
 function Home() {
   return (
     <div>
-        hi Home
+         <SubHome/>
         <Services/> 
     </div>
   )
